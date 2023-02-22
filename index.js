b9a20f57956bb5825828bbb687dfe424b9db9194
D-A-A-A <126110040+D-A-A-A@users.noreply.github.com>
2023-02-22 14:25:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Pense que dirias que no, pero bueno dijiste que si ahora podremos ser una linda parejita jaja te quiero domi ;D')
+});
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+    alert('Pense que dirias que si, pero bueno dijiste que no ahora se que no te gusto y que hice todo esto de por gusto ;c')
+});
+
+
